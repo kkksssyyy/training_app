378e57ac508ae774dbaaba3a0123124cb28127ea
kkksssyyy <kyoheisato0410@gmail.com>
2019-07-01 19:57:16 +0900

    Add a full_title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -9,13 +9,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get home" do
     get pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
