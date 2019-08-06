4c58b3bff5583e51348de176c9335d18f8d31286
kkksssyyy <kyoheisato0410@gmail.com>
2019-08-06 19:02:04 +0900

    user作成のroutesの修正
    users_controllerのnewとcreateアクションへのroutesが重複していたので修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get '/help', to: 'pages#help'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
