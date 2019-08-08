28c6eecf04bb0fe0ce29ad8fd27630fd29d220ef
kkksssyyy <kyoheisato0410@gmail.com>
2019-08-08 12:24:42 +0900

    routes修正
    以前、resources :usersで生成される名前付きルートに統一するように変更下が、
    signup時と、signup失敗時にusers/new画面にrenderされた時のurlが統一されない
    不具合？が発生したので、元に戻した。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/help', to: 'pages#help'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup infomation" do
-    get new_user_path
+    get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
                                   email: "user@invalid",
